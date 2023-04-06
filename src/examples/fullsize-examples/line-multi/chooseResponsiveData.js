e27ab67e6ca5ea42e4d954a394bc4f26fb326a51
David Egger <david.egga@gmx.at>
2023-04-06 14:47:16 +0200

    Refactor Multi Line Chart

--- src/examples/fullsize-examples/line-multi/chooseResponsiveData.js
+++ src/examples/fullsize-examples/line-multi/chooseResponsiveData.js
@@ -1,5 +1,5 @@
 import {findMatchingBoundsIndex} from "../../libs/respvis/respvis.js";
-import {select} from "../../libs/d3-7.6.0/d3.js";
+import {format, select} from "../../libs/d3-7.6.0/d3.js";
 
 const bounds = [
   {minWidth: '50rem', maxWidth: '60rem'},
@@ -22,4 +22,45 @@ export function chooseAxisFormat(axis, data) {
 
 function layoutAxisLabel(axis, orientation) {
   select(axis).selectAll('text').attr('orientation', orientation) //quick solution for flipping labels, apply actual styles in stylesheet
+}
+
+
+//------------------------------------------------------------
+
+
+const boundsChart = [
+  {minWidth: '50rem'},
+  {minWidth: '30rem'},
+]
+
+export function chooseResponsiveData(element, data) {
+  const index = findMatchingBoundsIndex(document.documentElement, boundsChart)
+
+  switch (index) {
+    case 0:
+      data.title = 'Electric Power Consumption (kWh per Capita)'
+      break
+    case 1:
+      data.title = 'Power Consumption (kWh)'
+      break
+    default:
+      data.title = 'Power (kWh)'
+  }
+
+  switch (index) {
+    case 0:
+      data.yAxis.configureAxis = (axis) => axis.tickFormat(format(','));
+      break
+    default:
+      data.yAxis.configureAxis = (axis) => axis.tickFormat(format('.2s'));
+      break
+  }
+
+  switch (index) {
+    case 0:
+      data.legend.title = 'Legend'
+      break
+    default:
+      data.legend.title = ''
+  }
 }
